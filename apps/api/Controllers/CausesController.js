a22208ecc9b643894275666d2ea0b2dbb849306c
Daniel Vu <dvv297@gmail.com>
2024-09-29 00:48:15 -0500

    Render pinata signed url

--- apps/api/Controllers/CausesController.js
+++ apps/api/Controllers/CausesController.js
@@ -1,12 +1,33 @@
 const { ObjectId } = require("mongodb");
+const { PinataSDK } = require("pinata");
 const { getCollection } = require("../mongo/MongoClient");
 const { BaseController } = require("./BaseController");
 
 class CausesController extends BaseController {
   async get(request, response) {
+    const pinata = new PinataSDK({
+      pinataJwt: process.env.PINATA_JWT,
+      pinataGateway: process.env.GATEWAY_URL,
+    });
     const collection = getCollection();
 
     const result = await collection.find({}).toArray();
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < result.length; i++) {
+      const item = result[i];
+      if (item.cid) {
+        // eslint-disable-next-line
+        const url = await pinata.gateways.createSignedURL({
+          cid: item.cid,
+          expires: 90,
+        });
+
+        console.log(url);
+        result[i].image = url;
+      } else {
+        item.image = "https://picsum.photos/200/300";
+      }
+    }
 
     return response.send({ data: result });
   }
